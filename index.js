3790095512b93521a9da0bb17307d1dd2b534d32
IoTcat <i@iotcat.me>
2021-07-10 19:02:02 +0800

    works

---
+++ index.js
@@ -0,0 +1,21 @@
+const wiot = require('wiot');
+
+
+let m = [];
+m[0] = new wiot.node.nodemcu('e31fd');
+
+let w = [];
+w[0] = new wiot.wire(0, true),
+w[1] = new wiot.wire(0),
+w[2] = new wiot.wire(0, true),
+w[3] = new wiot.wire();
+
+
+wiot.gpio(m[0], wiot.OUTPUT, m[0].D4, w[0]);
+
+wiot.buffer(m[0], w[0], w[2]);
+
+wiot.bigiot(m[0], 21249, 'ee37b3a2a', w[3], w[1]);
+
+wiot.operate(m[0], `${wiot.if(`$1=='play'`, 0, wiot.if(`$1=='stop'`, 1, `$0`))}`, w[2], w[3], w[1]);
+
