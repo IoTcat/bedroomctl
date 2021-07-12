4a32c943cff0b547a1d6917b1f43ca3f4fda217a
IoTcat <i@iotcat.me>
2021-07-12 15:20:37 +0800

    installed

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ w[2] = new wiot.wire(0, true),
 w[3] = new wiot.wire();
 
 
-wiot.gpio(m[0], wiot.OUTPUT, m[0].D4, w[0]);
+wiot.gpio(m[0], wiot.OUTPUT, m[0].D2, w[0]);
 
 wiot.buffer(m[0], w[0], w[2]);
 
 wiot.bigiot(m[0], 21249, 'ee37b3a2a', w[3], w[1]);
 
-wiot.operate(m[0], `${wiot.if(`$1=='play'`, 0, wiot.if(`$1=='stop'`, 1, `$0`))}`, w[2], w[3], w[1]);
+wiot.operate(m[0], `${wiot.if(`$1=='play'`, 1, wiot.if(`$1=='stop'`, 0, `$0`))}`, w[2], w[3], w[1]);
 
